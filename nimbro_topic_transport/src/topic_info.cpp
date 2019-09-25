d2f8b34f31bb6ccbf177693f52dd2d00e10328e7
Martin Pecka <peckama2@fel.cvut.cz>
2019-09-25 14:49:01 +0200

    Switched to using ros_babel_fish to get topic information.

--- nimbro_topic_transport/src/topic_info.cpp
+++ nimbro_topic_transport/src/topic_info.cpp
@@ -3,13 +3,7 @@
 
 #include "topic_info.h"
 
-#include <ros/names.h>
-
-#include <sys/wait.h>
-#include <stdio.h>
-#include <unistd.h>
-
-#include <string>
+#include <ros_babel_fish/generation/providers/integrated_description_provider.h>
 
 namespace nimbro_topic_transport
 {
@@ -17,85 +11,16 @@ namespace nimbro_topic_transport
 namespace topic_info
 {
 
-/**
- * Execute a rosmsg query and return result.
- *
- * The executed command is "rosmsg @a cmd @a type".
- *
- * @param stripNL If true, strip off the final newline.
- * @return rosmsg output
- **/
-static std::string msgQuery(const std::string& cmd, const std::string& type, bool stripNL)
-{
-	std::vector<char> buf(1024);
-	int idx = 0;
-
-	std::string error;
-	if(!ros::names::validate(type, error))
-	{
-		ROS_WARN("Got invalid message type '%s'", type.c_str());
-		return "";
-	}
-
-	int fds[2];
-	if(pipe(fds) != 0)
-		throw std::runtime_error("Could not create pipe");
-
-	int pid = fork();
-
-	if(pid == 0)
-	{
-		close(fds[0]);
-		dup2(fds[1], STDOUT_FILENO);
-
-		if(execlp("rosmsg", "rosmsg", cmd.c_str(), type.c_str(), nullptr) != 0)
-		{
-			throw std::runtime_error("Could not execlp() rosmsg");
-		}
-	}
-
-	close(fds[1]);
-
-	while(1)
-	{
-		buf.resize(idx + 1024);
-		int size = read(fds[0], buf.data() + idx, 1024);
-
-		if(size < 0)
-			throw std::runtime_error("Could not read()");
-		else if(size == 0)
-			break;
-
-		idx += size;
-	}
-
-	close(fds[0]);
-
-	int status;
-	waitpid(pid, &status, 0);
-
-	if(!WIFEXITED(status) || WEXITSTATUS(status) != 0 || idx == 0)
-	{
-		ROS_WARN("Could not get rosmsg %s for type '%s'", cmd.c_str(), type.c_str());
-		return "";
-	}
-
-	std::string ret(buf.data(), idx);
-
-	if(stripNL)
-		return std::string(buf.data(), idx-1);
-	else
-		return std::string(buf.data(), idx);
-}
+static ros_babel_fish::IntegratedDescriptionProvider descriptionProvider;
 
 std::string getMsgDef(const std::string& type)
 {
-	return msgQuery("show", type, false);
+  return descriptionProvider.getMessageDescription(type)->specification;
 }
 
 std::string getMd5Sum(const std::string& type)
 {
-	return msgQuery("md5", type, true);
+  return descriptionProvider.getMessageDescription(type)->md5;
 }
 
 void packMD5(const std::string& str, LEValue< 4 >* dest)
